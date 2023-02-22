cec917bdf7fd2e9256ea7ff21ca771bc2a7ef620
DP <drepcchannel@gmail.com>
2023-02-22 16:35:45 -0500

    updates mysql

--- server/dbinter.js
+++ server/dbinter.js
@@ -4,23 +4,38 @@
 
 */
 const pass = require('./password');
-
-var mysql = require('mysql');
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "test",
-  password: pass.getPass()
+  password: pass.getPass(),
+  port: "3454"
 });
 
+//may need to do encryption on both ends in future
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  let nameRec;
+  let passRec;
+  if (nameRec && passRec) {
+    let sql = `INSERT INTO userlogins (name, password) VALUES (${nameRec, passRec})`;
+    con.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log("creating user: " + nameRec);
+    });
+  } else {
+    console.log('name still null');
+  }
 });
 
+
+
 function getCreds (userName, passWord) {
-    console.log(username, passWord)
+    console.log(pass.getPass());
 }
+
+//check incoming for symbols, double check sql injections
 
 module.export = {getCreds}
--- server/index.js
+++ server/index.js
@@ -5,13 +5,15 @@ const dbinter= require('./dbinter');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors())
-
-dbinter.getCreds("hi", "there");
+app.use(cors());
 
 app.get("/api", (req, res) => {
   console.log('got req for api');
   res.json({message: "Hello"});
+});
+
+app.post("/createuser", (req, res) => {
+
 });
 
 app.listen(PORT, () => {
